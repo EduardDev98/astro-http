4778ff7cbd721d362346e9184fd715d41ed0b8eb
Carlos Moreno <cmorenodev98@gmail.com>
2025-05-20 17:48:19 -0600

    Uninstall lenis

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,15 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import react from '@astrojs/react';
-import cloudflare from '@astrojs/cloudflare';
+// import react from '@astrojs/react'; // Eliminar o comentar
 
 export default defineConfig({
   site: 'https://example.com',
   output: 'server',
   adapter: cloudflare(),
-  integrations: [mdx(), sitemap(), react()],
-  vite: {
-    ssr: {
-      external: ['react-dom/server'],
-      noExternal: ['@astrojs/react'],
-    },
-  },
+  integrations: [
+    mdx(),
+    sitemap(),
+    // react(),  // Eliminar o comentar
+  ],
 });
