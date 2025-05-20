8a5a8054ad10507dcb95a465d8793ede02e4f683
Carlos Moreno <cmorenodev98@gmail.com>
2025-05-20 16:55:33 -0600

    Add cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,12 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-import node from '@astrojs/node';
+// import node from '@astrojs/node';
 
 import react from '@astrojs/react';
+
+
+import cloudflare from '@astrojs/cloudflare';
 
 
 // https://astro.build/config
@@ -15,15 +18,16 @@ export default defineConfig({
 
   // output: "static" significa que Astro intentará prerenderizar todo por defecto
   // Cada página será exportada como HTML estático a menos que se indique lo contrario
-  
+
   // Si export const prerender = true; → fuerza la prerenderización de esa página (estática)
   // Si export const prerender = false; → esa página se generará dinámicamente en el servidor
 
-  output: "static",
-
   // Aunque uses output: "static", puedes usar un adaptador como Node
   // para soportar rutas dinámicas si alguna página tiene prerender = false
-  adapter: node({
-    mode: 'standalone',
-  }),
+  // adapter: node({
+  //   mode: 'standalone',
+  // }),
+  output: "static",
+
+  adapter: cloudflare(),
 });
